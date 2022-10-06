61b26e69b692585573ecf2cdf5fb19640f81dea4
Sergey Nikolaev <serj_nikolaev@yahoo.com>
2022-10-06 17:09:30 +0500

    first commit

---
+++ 3.py
@@ -0,0 +1,16 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://mfd.ru/currency/?currency=USD'
+response = requests.get(url)
+soup = BeautifulSoup(response.text, 'lxml')
+quotes = soup.find_all('span', class_='mfd-u' 'mdf-d')
+tags = soup.find_all('td', class_='tags')
+
+for i in range(0, len(quotes)):
+    print(quotes[i].text)
+    tagsforquote = tags[i].find_all('a', class_='tag')
+    for tagforquote in tagsforquote:
+        print(tagforquote.text)
+    print('\n')
+
